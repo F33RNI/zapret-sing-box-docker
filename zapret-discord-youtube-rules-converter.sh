fb3d1133398a8197fa3734f46395fcf9f50db109
F33RNI <fernlane@proton.me>
2025-08-26 20:48:18 +0300

    feat: test parsed rules

--- zapret-discord-youtube-rules-converter.sh
+++ zapret-discord-youtube-rules-converter.sh
@@ -26,9 +26,16 @@
 # This script parses latest rules from <https://github.com/Flowseal/zapret-discord-youtube>
 # And converts them into normal zapret NFQWS_OPT value
 
+# Specify test argument to this script to test each option
+if [ "$1" = "test" ]; then _test=true; fi
+
 REPO_URL="https://github.com/Flowseal/zapret-discord-youtube.git"
 TEMP_DIR="zapret-discord-youtube_temp"
 ZAPRET_DIR_INT="/opt/zapret"
+ZAPRET_CONFIG=${ZAPRET_CONFIG:-"configs/zapret.conf"}
+TEST_CONFIG=${TEST_CONFIG:-"configs/zapret.conf.test"}
+RELOAD_SCRIPT=${RELOAD_SCRIPT:-"reload.sh"}
+TEST_URL=${TEST_URL:-"https://youtube.com"}
 
 LOG_FILE="zapret-discord-youtube-rules-converter.log"
 
@@ -67,32 +74,132 @@ parse_bat() {
     filtered_lines=$(echo "$filtered_lines" | sed '$s/ --new$//')
 
     # Output final result
-    echo "# Parsed from $(basename "$bat_path") (Flowseal/zapret-discord-youtube @ $git_head)"
-    echo -e "NFQWS_OPT='\n${filtered_lines}\n'\n"
+    if [ "$_test" == true ]; then
+        echo "${filtered_lines}"
+    else
+        echo "# Parsed from $(basename "$bat_path") (Flowseal/zapret-discord-youtube @ $git_head)"
+        echo -e "NFQWS_OPT='\n${filtered_lines}\n'\n"
+    fi
+}
+
+# Parses NFQWS_OPT from .bat files and tests each file
+parse_test_bat() {
+    # Check test config file
+    if [ ! -f $TEST_CONFIG ]; then
+        echo "ERROR: No $TEST_CONFIG file!"
+        exit 1
+    fi
+
+    # Parse .bat file
+    local bat_path="$1"
+    nfqws_opt=$(parse_bat "$bat_path")
+    if [ -z "$nfqws_opt" ]; then exit 0; fi
+
+    # Log
+    echo -e "\n\nParsing and testing $(basename "$bat_path") (Flowseal/zapret-discord-youtube @ $git_head)..."
+    echo -e "NFQWS_OPT='\n${nfqws_opt}\n'"
+
+    # Rewrite NFQWS_OPT without new lines
+    nfqws_opt="${nfqws_opt//$'\n'/$' '}"
+
+    # Write test config
+    echo "Writing test config -> $ZAPRET_CONFIG"
+    config=$(sed 's|{NFQWS_OPT_PLACEHOLDER}|'"${nfqws_opt}"'|' "$TEST_CONFIG")
+    echo "$config" >"$ZAPRET_CONFIG"
+
+    # Reload and wait
+    echo "Calling reload script..."
+    bash "$RELOAD_SCRIPT" "$LOG_FILE" >>"$LOG_FILE"
+
+    # Test
+    echo "Testing $TEST_URL"
+    docker exec -t zapret-sing-box-docker curl --connect-timeout 1 --max-time 3 -L "$TEST_URL" >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "WORKING! WORKING! WORKING! ^^"
+    else
+        echo "Not working ˙◠˙"
+    fi
+
+    # Remove config
+    echo "Removing test config $ZAPRET_CONFIG"
+    rm "$ZAPRET_CONFIG"
 }
 
 # Clone repo
 if [ -d "$TEMP_DIR" ]; then
     echo "$TEMP_DIR already exists! Deleting..."
-    rm -rf "$TEMP_DIR"
+    #rm -rf "$TEMP_DIR"
 fi
-git clone "$REPO_URL" "$TEMP_DIR"
+#git clone "$REPO_URL" "$TEMP_DIR"
 
 # Save repo version for comments
 git_head=$(git -C "$TEMP_DIR" rev-parse --short HEAD)
 
-# Parse rules
-echo -e "\nParsing rules..."
-echo "1. Please set \`NFQWS_ENABLE\` in zapret.conf to \`1\`"
-echo "2. Please set \`NFQWS_PORTS_TCP\` in zapret.conf to \`80,443\`"
-echo "3. Please set \`NFQWS_PORTS_UDP\` in zapret.conf to \`443,50000-50100\`"
-echo "4. Copy parsed value of \`NFQWS_OPT\` from some script below into \`NFQWS_OPT\` in zapret.conf"
-echo -e "--------------------------------------------------------------------------------\n"
-export ZAPRET_DIR_INT="$ZAPRET_DIR_INT"
-export git_head="$git_head"
-export -f parse_bat
-find "$TEMP_DIR" -type f -name "*.bat" -exec bash -c 'parse_bat "$0"' {} \; | tee "$LOG_FILE"
-echo "--------------------------------------------------------------------------------"
+# Parse and test rules
+if [ "$_test" == true ]; then
+    # Check if container is running
+    container_id=$(docker ps | grep zapret-sing-box-docker | tail -n1 | awk '{print $1}')
+    if [ -z "$container_id" ]; then
+        echo "ERROR: Container not found! Please start it first!"
+        exit 1
+    fi
+
+    # Check if reload script exists
+    if [ ! -f "$RELOAD_SCRIPT" ]; then
+        echo "ERROR: Reload script $RELOAD_SCRIPT doesn't exist"
+        exit 1
+    fi
+
+    # Copy current config
+    if [ -f "$ZAPRET_CONFIG" ]; then
+        echo "Saving current config file $ZAPRET_CONFIG -> $ZAPRET_CONFIG.bak"
+        mv "$ZAPRET_CONFIG" "${ZAPRET_CONFIG}.bak"
+    fi
+
+    cleanup() {
+        # Restore original config file
+        if [ -f "${ZAPRET_CONFIG}.bak" ]; then
+            echo "Restoring config file $ZAPRET_CONFIG.bak -> $ZAPRET_CONFIG"
+            mv "${ZAPRET_CONFIG}.bak" "$ZAPRET_CONFIG"
+        fi
+
+        # Reload
+        echo "Calling reload script before exit... Please wait"
+        bash "$RELOAD_SCRIPT" "$LOG_FILE" >>"$LOG_FILE"
+        echo -e "\nFinished!"
+
+        exit 0
+    }
+
+    echo -e "\nParsing and testing rules..."
+    export LOG_FILE="$LOG_FILE"
+    export ZAPRET_DIR_INT="$ZAPRET_DIR_INT"
+    export git_head="$git_head"
+    export ZAPRET_CONFIG="$ZAPRET_CONFIG"
+    export TEST_CONFIG="$TEST_CONFIG"
+    export RELOAD_SCRIPT="$RELOAD_SCRIPT"
+    export TEST_URL="$TEST_URL"
+    export _test=true
+    export -f parse_bat
+    export -f parse_test_bat
+    trap cleanup SIGINT
+    find "$TEMP_DIR" -type f -name "*.bat" -exec bash -c 'parse_test_bat "$0"' {} \; | tee "$LOG_FILE"
+    cleanup
+
+# Just parse rules
+else
+    echo -e "\nParsing rules..."
+    echo "1. Please set \`NFQWS_ENABLE\` in zapret.conf to \`1\`"
+    echo "2. Please set \`NFQWS_PORTS_TCP\` in zapret.conf to \`80,443\`"
+    echo "3. Please set \`NFQWS_PORTS_UDP\` in zapret.conf to \`443,50000-50100\`"
+    echo "4. Copy parsed value of \`NFQWS_OPT\` from some script below into \`NFQWS_OPT\` in zapret.conf"
+    echo -e "--------------------------------------------------------------------------------\n"
+    export ZAPRET_DIR_INT="$ZAPRET_DIR_INT"
+    export git_head="$git_head"
+    export -f parse_bat
+    find "$TEMP_DIR" -type f -name "*.bat" -exec bash -c 'parse_bat "$0"' {} \; | tee "$LOG_FILE"
+    echo "--------------------------------------------------------------------------------"
+fi
 
 # Delete repo
-rm -rf "$TEMP_DIR"
+#rm -rf "$TEMP_DIR"
